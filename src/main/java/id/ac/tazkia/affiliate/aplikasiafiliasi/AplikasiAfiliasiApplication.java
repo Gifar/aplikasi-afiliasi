616e3566016832929361c6ece5355b656010e6cc
Endy Muhardin <endy.muhardin@gmail.com>
2017-10-18 06:53:12 +0700

    starter project

---
+++ src/main/java/id/ac/tazkia/affiliate/aplikasiafiliasi/AplikasiAfiliasiApplication.java
@@ -0,0 +1,12 @@
+package id.ac.tazkia.affiliate.aplikasiafiliasi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikasiAfiliasiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikasiAfiliasiApplication.class, args);
+	}
+}
---
+++ src/test/java/id/ac/tazkia/affiliate/aplikasiafiliasi/AplikasiAfiliasiApplicationTests.java
@@ -0,0 +1,16 @@
+package id.ac.tazkia.affiliate.aplikasiafiliasi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AplikasiAfiliasiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
